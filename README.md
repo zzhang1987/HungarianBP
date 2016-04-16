9ff2f36bf12311791f7fcacd62d44d926a6b7539
zhen <zhen@zzhang.org>
2016-04-16 10:38:46 +0800

    compiling code and README updated

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # HungarianBP
+
+The hungarian-BP code is contained in this repo.
+Please run compiling.m first.
--- compiling.m
+++ compiling.m
@@ -7,10 +7,12 @@ else
 end
 if(~exist('fgm', 'dir'))
     disp Please Download FGM package from http://www.f-zhou.com/gm_code.html
+    disp Please uncompress FGM package in the same directory as the script.
     return
 end
 if(~exist('Cars_and_Motorbikes_Dataset_and_Code','dir'))
     disp Please Download Cars and Motorbikes Dataset from https://sites.google.com/site/graphmatchingmethods/
+    disp Please uncompress Cars and Motorbikes Dataset in the same directory as the script.
 end
 cd fgm
 make
